14c710244fbf242b2f997288bf265c12704043c8
Abhyudya77 <23f3000013@es.study.iitm.ac.in>
2025-05-18 11:17:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./config/db.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+connectDB(); 
+
 const app = express();
 
 app.use(cors());
@@ -18,10 +20,6 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error(err));
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
