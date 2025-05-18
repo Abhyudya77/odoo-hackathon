47c7479b7759e843dd7f438ce76ee882cb9b31b1
Abhyudya77 <23f3000013@es.study.iitm.ac.in>
2025-05-18 16:21:39 +0530

    Create categories.js

---
+++ frontend/src/utils/categories.js
@@ -0,0 +1,10 @@
+const categories = [
+  "Garage Sales",
+  "Sports Matches",
+  "Community Classes",
+  "Volunteer Opportunities",
+  "Exhibitions",
+  "Festivals",
+];
+
+export default categories;
